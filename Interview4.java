7f69a62b1950d918edfec6ab249f0d62b5eee126
Aditya Kumar <sinhaaditya.2591@gmail.com>
2025-11-02 14:54:47 +0530

    Enhance Interview4.java with explanatory comments
    Added comments to explain the removeDuplicates logic.

--- Interview4.java
+++ Interview4.java
@@ -1,4 +1,15 @@
 // 80. Remove Duplicates from Sorted Array II
+// Use two pointers — one for reading (i), one for writing (j).
+// We only keep an element if it’s not equal to the element at position j-2.
+// Array is sorted ⇒ duplicates are consecutive.
+
+// Keep at most 2 of each element.
+
+// Compare current nums[i] with element nums[j - 2].
+
+// If different, copy it (safe to include).
+
+// If same, skip it (already 2 copies present).
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums.length <=2) return nums.length;
@@ -18,4 +29,5 @@ public class Interview4{
         int[] nums={0,0,1,1,1,1,2,3,3};
         System.out.print(s.removeDuplicates(nums));
     }
+
 }
