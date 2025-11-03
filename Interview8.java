5d480717af3832c878f4c1ca2b62426130c0c579
Aditya Kumar <sinhaaditya.2591@gmail.com>
2025-11-03 19:27:03 +0530

    Add files via upload

---
+++ Interview8.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseWords(String s) {
+       String[] words = s.split(" ");
+       StringBuilder reversed = new StringBuilder();
+       for(int i = words.length-1; i>=0; i--){
+        reversed.append(words[i]);
+        if(i!=0){
+            reversed.append(" ");
+        }
+       } 
+       return reversed.toString();
+    }
+}
+public class Interview8{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String str = "  Hello world from java  ";
+        System.out.print(s.reverseWords(str));
+    }
+}
